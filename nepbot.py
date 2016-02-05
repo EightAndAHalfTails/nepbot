8c4ba06303dfa3928de37e82195a5f96c858a05b
EightAndAHalfTails <jakebhumphrey@gmail.com>
2016-02-05 17:16:21 +0000

    added capture for PTU

--- nepbot.py
+++ nepbot.py
@@ -67,6 +67,119 @@ def cmd_pickfromrole(srv, role):
         return "I choose {0.mention}!".format(res)
     except:
         return "That's not a real role!"
+
+@client.event
+@asyncio.coroutine
+def cmd_capture(message):
+    cr = 100
+    while(True):
+        yield from client.send_message(message.channel, "What is your level?")
+        msg = yield from client.wait_for_message(author=message.author)
+        try:
+            cr += int(msg.content)
+            break
+        except:
+            yield from client.send_message(message.channel, "Sorry, what?")
+
+    while(True):
+        yield from client.send_message(message.channel, "What is the Pokemon's level?")
+        msg = yield from client.wait_for_message(author=message.author)
+        try:
+            cr -= 2*int(msg.content)
+            break
+        except:
+            yield from client.send_message(message.channel, "Sorry, what?")
+
+    while(True):
+        yield from client.send_message(message.channel, """Pick one of the following:```
+1: The Pokemon is at more than 75% Hit Points.
+2: The Pokemon is at 75% Hit Points or lower.
+3: The Pokemon is at 50% Hit Points or lower.
+4: The Pokemon is at 25% Hit Points or lower.
+5: The Pokemon is at exactly 1 Hit Point.```""")
+        msg = yield from client.wait_for_message(author=message.author)
+        try:
+            cr -= [30, 15, 0, -15, -30][int(msg.content)-1]
+            break
+        except:
+            yield from client.send_message(message.channel, "Sorry, what?")
+
+    while(True):
+        yield from client.send_message(message.channel, "How many evolutions does the Pokemon have remaining?")
+        msg = yield from client.wait_for_message(author=message.author)
+        try:
+            cr += [-10, 0, 10][int(msg.content)]
+            break
+        except:
+            yield from client.send_message(message.channel, "Sorry, what?")
+
+    while(True):
+        yield from client.send_message(message.channel, "Is the Pokemon Shiny?")
+        msg = yield from client.wait_for_message(author=message.author)
+        if msg.content.lower() in ['y', "yes"]:
+            cr -= 10
+            break
+        elif msg.content.lower() in ['n', "no"]:
+            break
+        else:
+            yield from client.send_message(message.channel, "Sorry, what?")
+            
+    while(True):
+        yield from client.send_message(message.channel, "Is the Pokemon Legendary?")
+        msg = yield from client.wait_for_message(author=message.author)
+        if msg.content.lower() in ['y', "yes"]:
+            cr -= 30
+            break
+        elif msg.content.lower() in ['n', "no"]:
+            break
+        else:
+            yield from client.send_message(message.channel, "Sorry, what?")
+
+    while(True):
+        yield from client.send_message(message.channel, "How many Persistant Status Afflictions (including Stuck) does the Pokemon have?")
+        msg = yield from client.wait_for_message(author=message.author)
+        try:
+            cr+= 10* int(msg.content)
+            break
+        except:
+            yield from client.send_message(message.channel, "Sorry, what?")
+        
+    while(True):
+        yield from client.send_message(message.channel, "How many Injuries or Volatile Status Afflictions (including Trapped) does the Pokemon have?")
+        msg = yield from client.wait_for_message(author=message.author)
+        try:
+            cr+= 5* int(msg.content)
+            break
+        except:
+            yield from client.send_message(message.channel, "Sorry, what?")
+
+    while(True):
+        yield from client.send_message(message.channel, "Did you crit the Accuracy Check?")
+        msg = yield from client.wait_for_message(author=message.author)
+        if msg.content.lower() in ['y', "yes"]:
+            cr += 10
+            break
+        elif msg.content.lower() in ['n', "no"]:
+            break
+        else:
+            yield from client.send_message(message.channel, "Sorry, what?")
+
+    while(True):
+        yield from client.send_message(message.channel, "Enter any other Bonuses as a positive number")
+        msg = yield from client.wait_for_message(author=message.author)
+        try:
+            cr += int(msg.content)
+            break
+        except:
+            yield from client.send_message(message.channel, "Sorry, what?")
+
+    yield from client.send_message(message.channel, "The Capture Rate is `{}`!".format(cr))
+    res = sum(roll(1, 100))
+    success = res == 100 or res <= cr
+    result  = "succeeded!" if success else "failed..."
+    yield from client.send_message(message.channel, "You {} `({})`".format(result, res))
+    return success
+    
     
 @client.event
 @asyncio.coroutine
@@ -101,5 +214,9 @@ def on_message(message):
             tmp = yield from client.send_message(message.channel, "Hmm...")
             res = yield from cmd_pickfromrole(message.server, parsed_cmd['role'])
             yield from client.edit_message(tmp, res)
+        if cmd == "capture":
+            print("That's a capture command!")
+            yield from cmd_capture(message)
+        
 
 client.run('jakebhumphrey@gmail.com', 'nepnepnishiteageru')

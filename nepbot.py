e961bd6efdb66a2b64e2e2c6977c12f10699aaf9
EightAndAHalfTails <jakebhumphrey@gmail.com>
2016-02-08 10:52:35 -0500

    refactoring to use the Bot class in ext.commands

--- nepbot.py
+++ nepbot.py
@@ -1,222 +1,194 @@
 import discord
+from discord.ext import commands
 import asyncio
 from parse import parse
 from random import randint, choice
 
-client = discord.Client()
+from credentials import email, passw
 
-@client.event
+desc = "The Protagonist of all Discord bots!"
+
+nepbot = commands.Bot(command_prefix=commands.when_mentioned, description=desc)
+
+@nepbot.event
 @asyncio.coroutine
 def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(nepbot.user.name)
+    print(nepbot.user.id)
     print('------')
 
-def roll(num, sides, mod=0):
+def _roll(num, sides, mod=0):
     print("Rolling {}d{}+{}".format(num, sides, mod))
     res =[ randint(1,sides) for i in range(num) ]
     if mod:
         res += [mod]
     return res
     
-@client.event
+@nepbot.command(description="Rolls dice given in NdN+N format.", brief="Rolls dice.")
 @asyncio.coroutine
-def cmd_roll(cmd, user):
-    cmd = cmd.replace(" ","")
+def roll(*cmd : str):
+    cmd = "".join(cmd)
     r = parse("{num:d}d{sides:d}",cmd)
     if r:
-        results = roll(r['num'], r['sides'])
+        results = _roll(r['num'], r['sides'])
     else:
         r = parse("{num:d}d{sides:d}{mod:d}",cmd)
         if r:
             print(r)
-            results = roll(r['num'], r['sides'], r['mod'])
+            results = _roll(r['num'], r['sides'], r['mod'])
         else:
-            return "Please give me a command in in NdN+N format!"
-    response = "_Rolling for {}_ `({})` ```{} ({})```".format(user.mention, cmd, sum(results), " + ".join(map(str, results)))
-    return response
+            yield from nepbot.reply("Please give me a command in in NdN+N format!")
+            return
+    yield from nepbot.reply("`({})` ```{} ({})```".format(cmd, sum(results), " + ".join(map(str, results))))
+    
 
-@client.event
+@nepbot.command(aliases=["pick"], description="Picks the <adj> out of [choices] (comma-separated)", brief="Let the hand of Nep decide...")
 @asyncio.coroutine
-def cmd_choose(choices, adj):
+def choose(adj:str, *choices:str):
+    print(adj, choices)
     try:
-        choices = [c.strip() for c in choices.split(',')]
+        choices = [c.strip() for c in " ".join(choices).split(',')]
+        print("Picking from {}".format(choices))
         res = choice(choices)
-        print("I choose {}".format(res))
-        return "{} is {}!".format(res, adj).capitalize()
+        print ("I choose {}".format(res))
+        yield from nepbot.reply("{} is the {}!".format(res, adj).capitalize())
     except:
-        return "I can't decide!"
+        yield from nepbot.reply("I can't decide!")
 
-@client.event
+@nepbot.command(aliases=["join"], help="Accepts an invite to a Discord server.")
 @asyncio.coroutine
-def cmd_accept(inv):
+def accept(invite_url:str):
     try:
-        yield from client.accept_invite(inv)
-        return "Thanks!"
+        yield from nepbot.accept_invite(invite_url)
+        yield from nepbot.reply("Thanks!")
     except:
-        return "It didn't work..."
+        yield from nepbot.reply("It didn't work...")
 
-@client.event
+@nepbot.command(pass_context=True, help="Picks one of the users in a given Role")
 @asyncio.coroutine
-def cmd_pickfromrole(srv, role):
+def delegate(ctx, *role:str):
+    role = " ".join(role).lower()
+    print(role)
     try:
-        usrs = [ u for u in srv.members if role in [r.name.lower() for r in u.roles] ]
+        usrs = [ u for u in ctx.message.server.members if role in [r.name.lower() for r in u.roles] ]
         res = choice(usrs)
         print("I choose {0.name}!".format(res))
-        return "I choose {0.mention}!".format(res)
+        yield from nepbot.say("I choose {0.mention}!".format(res))
     except:
-        return "That's not a real role!"
+        yield from nepbot.say("That's not a real role!")
 
-@client.event
+@nepbot.command(pass_context=True, help="Try to capture a Pokemon in PTU")
 @asyncio.coroutine
-def cmd_capture(message):
+def capture(ctx):
+    message = ctx.message
     cr = 100
     while(True):
-        yield from client.send_message(message.channel, "What is your level?")
-        msg = yield from client.wait_for_message(author=message.author)
+        yield from nepbot.reply("What is your level?")
+        msg = yield from nepbot.wait_for_message(author=message.author)
         try:
             cr += int(msg.content)
             break
         except:
-            yield from client.send_message(message.channel, "Sorry, what?")
+            yield from nepbot.reply("Sorry, what?")
 
     while(True):
-        yield from client.send_message(message.channel, "What is the Pokemon's level?")
-        msg = yield from client.wait_for_message(author=message.author)
+        yield from nepbot.reply("What is the Pokemon's level?")
+        msg = yield from nepbot.wait_for_message(author=message.author)
         try:
             cr -= 2*int(msg.content)
             break
         except:
-            yield from client.send_message(message.channel, "Sorry, what?")
+            yield from nepbot.reply("Sorry, what?")
 
     while(True):
-        yield from client.send_message(message.channel, """Pick one of the following:```
+        yield from nepbot.reply("""Pick one of the following:```
 1: The Pokemon is at more than 75% Hit Points.
 2: The Pokemon is at 75% Hit Points or lower.
 3: The Pokemon is at 50% Hit Points or lower.
 4: The Pokemon is at 25% Hit Points or lower.
 5: The Pokemon is at exactly 1 Hit Point.```""")
-        msg = yield from client.wait_for_message(author=message.author)
+        msg = yield from nepbot.wait_for_message(author=message.author)
         try:
             cr -= [30, 15, 0, -15, -30][int(msg.content)-1]
             break
         except:
-            yield from client.send_message(message.channel, "Sorry, what?")
+            yield from nepbot.reply("Sorry, what?")
 
     while(True):
-        yield from client.send_message(message.channel, "How many evolutions does the Pokemon have remaining?")
-        msg = yield from client.wait_for_message(author=message.author)
+        yield from nepbot.reply("How many evolutions does the Pokemon have remaining?")
+        msg = yield from nepbot.wait_for_message(author=message.author)
         try:
             cr += [-10, 0, 10][int(msg.content)]
             break
         except:
-            yield from client.send_message(message.channel, "Sorry, what?")
+            yield from nepbot.reply("Sorry, what?")
 
     while(True):
-        yield from client.send_message(message.channel, "Is the Pokemon Shiny?")
-        msg = yield from client.wait_for_message(author=message.author)
+        yield from nepbot.reply("Is the Pokemon Shiny?")
+        msg = yield from nepbot.wait_for_message(author=message.author)
         if msg.content.lower() in ['y', "yes"]:
             cr -= 10
             break
         elif msg.content.lower() in ['n', "no"]:
             break
         else:
-            yield from client.send_message(message.channel, "Sorry, what?")
+            yield from nepbot.reply("Sorry, what?")
             
     while(True):
-        yield from client.send_message(message.channel, "Is the Pokemon Legendary?")
-        msg = yield from client.wait_for_message(author=message.author)
+        yield from nepbot.reply("Is the Pokemon Legendary?")
+        msg = yield from nepbot.wait_for_message(author=message.author)
         if msg.content.lower() in ['y', "yes"]:
             cr -= 30
             break
         elif msg.content.lower() in ['n', "no"]:
             break
         else:
-            yield from client.send_message(message.channel, "Sorry, what?")
+            yield from nepbot.reply("Sorry, what?")
 
     while(True):
-        yield from client.send_message(message.channel, "How many Persistant Status Afflictions (including Stuck) does the Pokemon have?")
-        msg = yield from client.wait_for_message(author=message.author)
+        yield from nepbot.reply("How many Persistant Status Afflictions (including Stuck) does the Pokemon have?")
+        msg = yield from nepbot.wait_for_message(author=message.author)
         try:
             cr+= 10* int(msg.content)
             break
         except:
-            yield from client.send_message(message.channel, "Sorry, what?")
+            yield from nepbot.reply("Sorry, what?")
         
     while(True):
-        yield from client.send_message(message.channel, "How many Injuries or Volatile Status Afflictions (including Trapped) does the Pokemon have?")
-        msg = yield from client.wait_for_message(author=message.author)
+        yield from nepbot.reply("How many Injuries or Volatile Status Afflictions (including Trapped) does the Pokemon have?")
+        msg = yield from nepbot.wait_for_message(author=message.author)
         try:
             cr+= 5* int(msg.content)
             break
         except:
-            yield from client.send_message(message.channel, "Sorry, what?")
+            yield from nepbot.reply("Sorry, what?")
 
     while(True):
-        yield from client.send_message(message.channel, "Did you crit the Accuracy Check?")
-        msg = yield from client.wait_for_message(author=message.author)
+        yield from nepbot.reply("Did you crit the Accuracy Check?")
+        msg = yield from nepbot.wait_for_message(author=message.author)
         if msg.content.lower() in ['y', "yes"]:
             cr += 10
             break
         elif msg.content.lower() in ['n', "no"]:
             break
         else:
-            yield from client.send_message(message.channel, "Sorry, what?")
+            yield from nepbot.reply("Sorry, what?")
 
     while(True):
-        yield from client.send_message(message.channel, "Enter any other Bonuses as a positive number")
-        msg = yield from client.wait_for_message(author=message.author)
+        yield from nepbot.reply("Enter any other Bonuses as a positive number")
+        msg = yield from nepbot.wait_for_message(author=message.author)
         try:
             cr += int(msg.content)
             break
         except:
-            yield from client.send_message(message.channel, "Sorry, what?")
+            yield from nepbot.reply("Sorry, what?")
 
-    yield from client.send_message(message.channel, "The Capture Rate is `{}`!".format(cr))
-    res = sum(roll(1, 100))
+    yield from nepbot.reply("The Capture Rate is `{}`!".format(cr))
+    res = sum(_roll(1, 100))
     success = res == 100 or res <= cr
     result  = "succeeded!" if success else "failed..."
-    yield from client.send_message(message.channel, "You {} `({})`".format(result, res))
+    yield from nepbot.reply("You {} `({})`".format(result, res))
     return success
     
-    
-@client.event
-@asyncio.coroutine
-def on_message(message):
-    #print("Message recieved: {}".format(message.content))
-    if client.user in message.mentions:
-        print("That's me!")
-        cmd = message.content.replace("<@{}>".format(client.user.id),"").strip().lower()
-        print("I'm being told to '{}'".format(cmd))
-        parsed_cmd = parse("roll {cmd}", cmd)
-        if parsed_cmd:
-            print("That's a roll command!")
-            tmp = yield from client.send_message(message.channel, "Rolling...")
-            res = yield from cmd_roll(parsed_cmd['cmd'], message.author)
-            yield from client.edit_message(tmp, res)
-        parsed_cmd = parse("which is {adj}:{list}?", cmd)
-        if parsed_cmd:
-            print("That's a choose command!")
-            yield from client.send_message(message.channel, "I have to pick, huh?")
-            tmp = yield from client.send_message(message.channel, "Hmm...")
-            res = yield from cmd_choose(parsed_cmd['list'], parsed_cmd['adj'])
-            yield from client.edit_message(tmp, res)
-        parsed_cmd = parse("accept {inv}", cmd)
-        if parsed_cmd:
-            print("I'm being given an invite!")
-            res = yield from cmd_accept(parsed_cmd['inv'])
-            yield from client.send_message(message.author, res)
-        parsed_cmd = parse("pick one of the {role}",cmd)
-        if parsed_cmd:
-            print("That's a pickfromrole command!")
-            yield from client.send_message(message.channel, "I have to pick, huh?")
-            tmp = yield from client.send_message(message.channel, "Hmm...")
-            res = yield from cmd_pickfromrole(message.server, parsed_cmd['role'])
-            yield from client.edit_message(tmp, res)
-        if cmd == "capture":
-            print("That's a capture command!")
-            yield from cmd_capture(message)
-        
-
-client.run('jakebhumphrey@gmail.com', 'nepnepnishiteageru')
+nepbot.run(email, passw)

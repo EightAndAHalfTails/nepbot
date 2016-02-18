357b3369425ad0066fc470bb5d8a0e7284161a81
EightAndAHalfTails <jakebhumphrey@gmail.com>
2016-02-18 17:00:13 -0500

    added speak command

--- nepbot.py
+++ nepbot.py
@@ -7,14 +7,18 @@ from datetime import date
 import requests
 from lxml import html
 from credentials import email, passw, irclogdir
+from gtts import gTTS
+from tempfile import TemporaryFile
+from time import sleep
+import os
+import io
 
 desc = "The Protagonist of all Discord bots!"
 
 nepbot = commands.Bot(command_prefix=commands.when_mentioned, description=desc)
 
 @nepbot.event
-@asyncio.coroutine
-def on_ready():
+async def on_ready():
     print('Logged in as')
     print(nepbot.user.name)
     print(nepbot.user.id)
@@ -28,8 +32,7 @@ def _roll(num, sides, mod=0):
     return res
     
 @nepbot.command(description="Rolls dice given in NdN+N format.", brief="Rolls dice.")
-@asyncio.coroutine
-def roll(*cmd : str):
+async def roll(*cmd : str):
     cmd = "".join(cmd)
     r = parse("{num:d}d{sides:d}",cmd)
     print(r)
@@ -41,184 +44,211 @@ def roll(*cmd : str):
         if r:
             results = _roll(r['num'], r['sides'], r['mod'])
         else:
-            yield from nepbot.reply("Please give me a command in in NdN+N format!")
+            await nepbot.reply("Please give me a command in in NdN+N format!")
             return
-    yield from nepbot.reply("`({})` ```{} ({})```".format(cmd, sum(results), " + ".join(map(str, results))))
+    await nepbot.reply("`({})` ```{} ({})```".format(cmd, sum(results), " + ".join(map(str, results))))
     
 
 @nepbot.command(aliases=["pick"], description="Picks the <adj> out of [choices] (comma-separated)", brief="Let the hand of Nep decide...")
-@asyncio.coroutine
-def choose(adj:str, *choices:str):
+async def choose(adj:str, *choices:str):
     print(adj, choices)
     try:
         choices = [c.strip() for c in " ".join(choices).split(',')]
         print("Picking from {}".format(choices))
         res = choice(choices)
         print ("I choose {}".format(res))
-        yield from nepbot.reply("{} is the {}!".format(res, adj).capitalize())
+        await nepbot.reply("{} is the {}!".format(res, adj).capitalize())
     except:
-        yield from nepbot.reply("I can't decide!")
+        await nepbot.reply("I can't decide!")
 
 @nepbot.command(aliases=["join"], help="Accepts an invite to a Discord server.")
-@asyncio.coroutine
-def accept(invite_url:str):
+async def accept(invite_url:str):
     try:
-        yield from nepbot.accept_invite(invite_url)
-        yield from nepbot.reply("Thanks!")
+        await nepbot.accept_invite(invite_url)
+        await nepbot.reply("Thanks!")
     except:
-        yield from nepbot.reply("It didn't work...")
+        await nepbot.reply("It didn't work...")
 
 @nepbot.command(pass_context=True, help="Picks one of the users in a given Role")
-@asyncio.coroutine
-def delegate(ctx, *role:str):
+async def delegate(ctx, *role:str):
     role = " ".join(role).lower()
     print(role)
     try:
         usrs = [ u for u in ctx.message.server.members if role in [r.name.lower() for r in u.roles] ]
         res = choice(usrs)
         print("I choose {0.name}!".format(res))
-        yield from nepbot.say("I choose {0.mention}!".format(res))
+        await nepbot.say("I choose {0.mention}!".format(res))
     except:
-        yield from nepbot.say("That's not a real role!")
+        await nepbot.say("That's not a real role!")
 
 @nepbot.command(pass_context=True, help="Try to capture a Pokemon in PTU")
-@asyncio.coroutine
-def capture(ctx):
+async def capture(ctx):
     message = ctx.message
     cr = 100
     while(True):
-        yield from nepbot.reply("What is your level?")
-        msg = yield from nepbot.wait_for_message(author=message.author)
+        await nepbot.reply("What is your level?")
+        msg = await nepbot.wait_for_message(author=message.author)
         try:
             cr += int(msg.content)
             break
         except:
-            yield from nepbot.reply("Sorry, what?")
+            await nepbot.reply("Sorry, what?")
 
     while(True):
-        yield from nepbot.reply("What is the Pokemon's level?")
-        msg = yield from nepbot.wait_for_message(author=message.author)
+        await nepbot.reply("What is the Pokemon's level?")
+        msg = await nepbot.wait_for_message(author=message.author)
         try:
             cr -= 2*int(msg.content)
             break
         except:
-            yield from nepbot.reply("Sorry, what?")
+            await nepbot.reply("Sorry, what?")
 
     while(True):
-        yield from nepbot.reply("""Pick one of the following:```
+        await nepbot.reply("""Pick one of the following:```
 1: The Pokemon is at more than 75% Hit Points.
 2: The Pokemon is at 75% Hit Points or lower.
 3: The Pokemon is at 50% Hit Points or lower.
 4: The Pokemon is at 25% Hit Points or lower.
 5: The Pokemon is at exactly 1 Hit Point.```""")
-        msg = yield from nepbot.wait_for_message(author=message.author)
+        msg = await nepbot.wait_for_message(author=message.author)
         try:
             cr -= [30, 15, 0, -15, -30][int(msg.content)-1]
             break
         except:
-            yield from nepbot.reply("Sorry, what?")
+            await nepbot.reply("Sorry, what?")
 
     while(True):
-        yield from nepbot.reply("How many evolutions does the Pokemon have remaining?")
-        msg = yield from nepbot.wait_for_message(author=message.author)
+        await nepbot.reply("How many evolutions does the Pokemon have remaining?")
+        msg = await nepbot.wait_for_message(author=message.author)
         try:
             cr += [-10, 0, 10][int(msg.content)]
             break
         except:
-            yield from nepbot.reply("Sorry, what?")
+            await nepbot.reply("Sorry, what?")
 
     while(True):
-        yield from nepbot.reply("Is the Pokemon Shiny?")
-        msg = yield from nepbot.wait_for_message(author=message.author)
+        await nepbot.reply("Is the Pokemon Shiny?")
+        msg = await nepbot.wait_for_message(author=message.author)
         if msg.content.lower() in ['y', "yes"]:
             cr -= 10
             break
         elif msg.content.lower() in ['n', "no"]:
             break
         else:
-            yield from nepbot.reply("Sorry, what?")
+            await nepbot.reply("Sorry, what?")
             
     while(True):
-        yield from nepbot.reply("Is the Pokemon Legendary?")
-        msg = yield from nepbot.wait_for_message(author=message.author)
+        await nepbot.reply("Is the Pokemon Legendary?")
+        msg = await nepbot.wait_for_message(author=message.author)
         if msg.content.lower() in ['y', "yes"]:
             cr -= 30
             break
         elif msg.content.lower() in ['n', "no"]:
             break
         else:
-            yield from nepbot.reply("Sorry, what?")
+            await nepbot.reply("Sorry, what?")
 
     while(True):
-        yield from nepbot.reply("How many Persistant Status Afflictions (including Stuck) does the Pokemon have?")
-        msg = yield from nepbot.wait_for_message(author=message.author)
+        await nepbot.reply("How many Persistant Status Afflictions (including Stuck) does the Pokemon have?")
+        msg = await nepbot.wait_for_message(author=message.author)
         try:
             cr+= 10* int(msg.content)
             break
         except:
-            yield from nepbot.reply("Sorry, what?")
+            await nepbot.reply("Sorry, what?")
         
     while(True):
-        yield from nepbot.reply("How many Injuries or Volatile Status Afflictions (including Trapped) does the Pokemon have?")
-        msg = yield from nepbot.wait_for_message(author=message.author)
+        await nepbot.reply("How many Injuries or Volatile Status Afflictions (including Trapped) does the Pokemon have?")
+        msg = await nepbot.wait_for_message(author=message.author)
         try:
             cr+= 5* int(msg.content)
             break
         except:
-            yield from nepbot.reply("Sorry, what?")
+            await nepbot.reply("Sorry, what?")
 
     while(True):
-        yield from nepbot.reply("Did you crit the Accuracy Check?")
-        msg = yield from nepbot.wait_for_message(author=message.author)
+        await nepbot.reply("Did you crit the Accuracy Check?")
+        msg = await nepbot.wait_for_message(author=message.author)
         if msg.content.lower() in ['y', "yes"]:
             cr += 10
             break
         elif msg.content.lower() in ['n', "no"]:
             break
         else:
-            yield from nepbot.reply("Sorry, what?")
+            await nepbot.reply("Sorry, what?")
 
     while(True):
-        yield from nepbot.reply("Enter any other Bonuses as a positive number")
-        msg = yield from nepbot.wait_for_message(author=message.author)
+        await nepbot.reply("Enter any other Bonuses as a positive number")
+        msg = await nepbot.wait_for_message(author=message.author)
         try:
             cr += int(msg.content)
             break
         except:
-            yield from nepbot.reply("Sorry, what?")
+            await nepbot.reply("Sorry, what?")
 
-    yield from nepbot.reply("The Capture Rate is `{}`!".format(cr))
+    await nepbot.reply("The Capture Rate is `{}`!".format(cr))
     res = sum(_roll(1, 100))
     success = res == 100 or res <= cr
     result  = "succeeded!" if success else "failed..."
-    yield from nepbot.reply("You {} `({})`".format(result, res))
+    await nepbot.reply("You {} `({})`".format(result, res))
     
 @nepbot.command(help="Offers a glimpse into the Hyperdimension...")
-@asyncio.coroutine
-def scry():
+async def scry():
     try:
         today = date.today()
         log = "{}/freenode_##compsoc-uk-anime_{}{:02}{:02}.log".format(irclogdir, today.year, today.month, today.day)
         with open(log) as f:
-            yield from nepbot.say("`{}`".format(f.readlines()[-1]))
+            await nepbot.say("`{}`".format(f.readlines()[-1]))
     except:
-        yield from nepbot.say("Outlook unclear. Try again later.")
+        await nepbot.say("Outlook unclear. Try again later.")
 
 @nepbot.command(help="Posts an xkcd comic and hovertext")
-@asyncio.coroutine
-def xkcd(number:str=""):
+async def xkcd(number:str=""):
     try:
         page = requests.get('http://xkcd.com/{}'.format(number))
         tree = html.fromstring(page.content)
-
+        
         comic = tree.xpath('//div[@id="comic"]/img/attribute::src')[0]
         hover = tree.xpath('//div[@id="comic"]/img/attribute::title')[0]
         comic = requests.get("http:{}".format(comic)).content
-
-        yield from nepbot.upload(comic, "xkcd-{}.png".format(number))
-        yield from nepbot.say("_{}_".format(hover))
+        
+        await nepbot.upload(io.BytesIO(comic), "xkcd-{}.png".format(number))
+        await nepbot.say("_{}_".format(hover))
     except Exception as e:
         print(e)
-        yield from nepbot.say("I failed...")
+        await nepbot.say("I failed...")
+
+@nepbot.command(help="Says something in a voice channel")
+async def speak(server:str, channel:str, *message:str):
+    chan = discord.utils.get(nepbot.get_all_channels(), server__name=server, name=channel, type=discord.ChannelType.voice)
+    if not chan:
+        await nepbot.say("I can't find that voice channel!")
+        return
+
+    sentence = " ".join(message)
+    print(sentence)
+    tts = gTTS(text=sentence, lang='ja')
+    #f = TemporaryFile()
+    #tts.write_to_fp(f)
+    tts.save("nepsay.mp3")
+
+    try:
+        voice = await nepbot.join_voice_channel(chan)
+        player = voice.create_ffmpeg_player("nepsay.mp3")
+        #player = voice.create_ffmpeg_player(f, options="-v verbose", pipe=True) # TODO: get this to work
+
+        player.start()
+        while not player.is_done():
+            await asyncio.sleep(1)
+    except Exception as e:
+        print(e)
+        await nepbot.say("I failed...")
+    finally:
+        #f.close()
+        try:
+            os.remove("nepsay.mp3")
+        except:
+            pass
+        await voice.disconnect()
 
 nepbot.run(email, passw)
